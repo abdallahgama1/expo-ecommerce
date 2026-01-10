d407802e11d4d4fd27323a00ff76a983112b7237
Abdallah Gamal <abdallahgamal20022@gmail.com>
2026-01-10 13:45:08 +0200

    DB config and tool setup

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -5,4 +5,9 @@ export const ENV = {
     NODE_ENV: process.env.NODE_ENV || "development",
     PORT: process.env.PORT || 3000,
     DB_URI: process.env.DB_URI || "mongodb://localhost:27017/myapp",
+    CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY || "",
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY || "",
+    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME || "",
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY || "",
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET || "",
 };
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,15 @@
 import express from "express";
 import path from "path";
 import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
+import { clerkMiddleware } from "@clerk/express";
+
 const app = express();
 
 const __dirname = path.resolve();
+
+app.use(express.json());
+app.use(clerkMiddleware());
 
 app.get("/api", (req, res) => {
     res.status(200).json({ message: "Welcome to the server" });
@@ -19,4 +25,5 @@ if (ENV.NODE_ENV === "production") {
 
 app.listen(ENV.PORT, () => {
     console.log(`server is up and running on port ${ENV.PORT} `);
+    connectDB();
 });
