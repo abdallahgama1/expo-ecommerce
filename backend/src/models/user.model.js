81cde294232f272300c76ae1f1def0687d53bc47
Abdallah Gamal <abdallahgamal20022@gmail.com>
2026-01-11 16:53:22 +0200

    add user model and clerk setup

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,70 @@
+import mongoose from "mongoose";
+
+const addresseSchema = new mongoose.Schema(
+    {
+        lable: {
+            type: String,
+            required: true,
+        },
+        fullName: {
+            type: String,
+            required: true,
+        },
+        streetAddress: {
+            type: String,
+            required: true,
+        },
+        state: {
+            type: String,
+            required: true,
+        },
+        zipCode: {
+            type: String,
+            required: true,
+        },
+        phoneNumber: {
+            type: String,
+            required: true,
+        },
+        isDefault: {
+            type: Boolean,
+            required: true,
+            default: false,
+        },
+    },
+    { timestamps: true }
+);
+
+const userSchema = new mongoose.Schema(
+    {
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+        imageUrl: {
+            type: String,
+            required: false,
+            default: "",
+        },
+        clerkId: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        addresses: [addresseSchema],
+        wishList: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Product",
+            },
+        ],
+    },
+    { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);
