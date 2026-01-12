5dc12d154916b487e6a526557daae1a193510559
Abdallah Gamal <abdallahgamal20022@gmail.com>
2026-01-12 16:46:52 +0200

    auth implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,8 @@ import path from "path";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 import { clerkMiddleware } from "@clerk/express";
+import { serve } from "inngest/express";
+import { functions, inngest } from "./config/inngest.js";
 
 const app = express();
 
@@ -10,6 +12,8 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(clerkMiddleware());
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get("/api", (req, res) => {
     res.status(200).json({ message: "Welcome to the server" });
